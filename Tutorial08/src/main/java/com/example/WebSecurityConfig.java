767676ea9aea1959bca96a5eb8e7408965259e94
DeptKrimDosen\Guest Account <pbprameshwara@gmail.com>
2017-11-19 00:02:01 +0700

    FINAL COMMIT

--- Tutorial08/src/main/java/com/example/WebSecurityConfig.java
+++ Tutorial08/src/main/java/com/example/WebSecurityConfig.java
@@ -14,13 +14,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/course/**").hasRole("ADMIN").antMatchers("/student/**").hasRole("USER").anyRequest().authenticated().and().formLogin()
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/course/**").hasRole("ADMIN")
+				.antMatchers("/student/**").hasRole("USER").anyRequest().authenticated().and().formLogin()
 				.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
+	// @Autowired
+	// public void configureGlobal(AuthenticationManagerBuilder auth) throws
+	// Exception {
+	// auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	// auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+	// }
+
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+	DataSource dataSource;
+
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username,password,enabled from users where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
+
 }
